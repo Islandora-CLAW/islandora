67bf6ca002f61f923aa8294e595e6130f674e773
Mark Jordan <mjordan@sfu.ca>
2019-09-10 14:32:31 +0000

    Remove cruft.

--- src/Plugin/Condition/NodeIsIslandoraObject.php
+++ src/Plugin/Condition/NodeIsIslandoraObject.php
@@ -3,9 +3,7 @@
 namespace Drupal\islandora\Plugin\Condition;
 
 use Drupal\Core\Condition\ConditionPluginBase;
-use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\islandora\IslandoraUtils;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -20,49 +18,6 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * )
  */
 class NodeIsIslandoraObject extends ConditionPluginBase implements ContainerFactoryPluginInterface {
-
-  /**
-   * Islandora utils.
-   *
-   * @var \Drupal\islandora\IslandoraUtils
-   */
-  protected $utils;
-
-  /**
-   * Term storage.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManager
-   */
-  protected $entityTypeManager;
-
-  /**
-   * Constructor.
-   *
-   * @param array $configuration
-   *   The plugin configuration, i.e. an array with configuration values keyed
-   *   by configuration option name. The special key 'context' may be used to
-   *   initialize the defined contexts by setting it to an array of context
-   *   values keyed by context names.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\islandora\IslandoraUtils $utils
-   *   Islandora utils.
-   * @param \Drupal\Core\Entity\EntityTypeManager $entity_type_manager
-   *   Entity type manager.
-   */
-  public function __construct(
-    array $configuration,
-    $plugin_id,
-    $plugin_definition,
-    IslandoraUtils $utils,
-    EntityTypeManager $entity_type_manager
-  ) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->utils = $utils;
-    $this->entityTypeManager = $entity_type_manager;
-  }
 
   /**
    * {@inheritdoc}
